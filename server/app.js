99c1b330c22f46940b8e0c24f0c7f65f7942bc1c
zhengchunguang <1083466294@qq.com>
2018-12-05 17:25:42 +0800

    111

--- server/app.js
+++ server/app.js
@@ -1,21 +1,24 @@
 const Koa=require('koa');
 const Static=require('koa-static-cache');
 const Router=require('koa-router');
+const fs=require('fs');
 
 const userRouter=require('./routers/user');
 
 const app=new Koa();
 const router=new Router();
-
-
+const content=fs.readFileSync(__dirname+'/static/index.html');
+app.use(Static(__dirname+'/static',{
+	prefix:'/'
+}))
 router.use('/user',userRouter.routes());
-
-
+router.get('/',ctx=>{
+	
+	ctx.body=content.toString();
+})
 
 app.use(router.routes());
-app.use(Static(__dirname+'/static',{
-	prefix:'/static'
-}))
+
 
 app.listen(80,()=>{
 	console.log("服务启动成功！")
